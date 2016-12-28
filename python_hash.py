82dfde69bd8c3f757381231987d17317c4bcdbb6
Drew Ballantine <Drew Ballantine>
2016-12-28 15:26:39 -0500

    Updated if statements with the right syntax

--- python_hash.py
+++ python_hash.py
@@ -12,22 +12,22 @@ option = 0
 while option != 1 or option !=2 or option !=3 or option !=4 or option !=5 or option !=6:
     option = input('Please select what encryption level you would like:\n1. SHA512\n2. SHA256\n3. SHA128\n4. SHA2\n 5. SHA1\n 6. MD5 ')
 
-    if option = 1:
+    if option == 1:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA512)))
-    elif option = 2:
+    elif option == 2:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA256)))
-    elif option = 3:
+    elif option == 3:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA128)))
-    elif option = 4:
+    elif option == 4:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA2)))
-    elif option = 5:
+    elif option == 5:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_SHA1)))
-    elif option = 6:
+    elif option == 6:
         password = getpass.getpass("Please enter the password you wish to hash: ")
         print(crypt.crypt(str(password), crypt.mksalt(crypt.METHOD_MD5)))
     else:
